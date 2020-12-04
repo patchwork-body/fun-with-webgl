c5b895a58edf5c4634d5867fe6730c7d5689627a
gugfug <gugfug1@gmail.com>
2020-12-04 07:16:15 +0000

    colored point

--- example/index.ts
+++ example/index.ts
@@ -23,8 +23,30 @@ mouse.register((x, y) => {
       name: `red-point-${++index}`,
       position: [x, y, 0.0],
       size: 10.0,
+      color: defineColor(x, y),
     }),
   );
 });
+
+const defineColor = (x: number, y: number): [number, number, number] => {
+  const RED = [1.0, 0.0, 0.0];
+  const GREEN = [0.0, 1.0, 0.0];
+  const BLUE = [0.0, 0.0, 1.0];
+  const YELLOW = [1.0, 1.0, 0.0];
+
+  let color;
+
+  if (x > 0 && y > 0) {
+    color = RED;
+  } else if (x < 0 && y > 0) {
+    color = YELLOW;
+  } else if (x > 0 && y < 0) {
+    color = GREEN;
+  } else {
+    color = BLUE;
+  }
+
+  return color as [number, number, number];
+};
 
 world.startScene('red-point-scene');
--- src/core/primitives/point.ts
+++ src/core/primitives/point.ts
@@ -10,17 +10,20 @@ import { Component, IComponentConfig } from '../component';
 interface IPointConfig extends IComponentConfig {
   position: [number, number, number];
   size: number;
+  color: [number, number, number];
 }
 
 class Point extends Component {
   private _position: [number, number, number] = [0.0, 0.0, 0.0];
   private _size = 1.0;
+  private _color: [number, number, number] = [0.0, 0.0, 0.0];
 
-  constructor({ name, position, size }: IPointConfig) {
+  constructor({ name, position, size, color }: IPointConfig) {
     super({ name });
 
     this._position = position;
     this._size = size;
+    this._color = color;
   }
 
   render(gl: WebGL2RenderingContext): void {
@@ -32,9 +35,11 @@ class Point extends Component {
 
     const pointPosition = gl.getAttribLocation(program, 'a_PointPosition');
     const pointSize = gl.getAttribLocation(program, 'a_PointSize');
+    const pointColor = gl.getUniformLocation(program, 'u_PointColor');
 
     gl.vertexAttrib3f(pointPosition, ...this._position);
     gl.vertexAttrib1f(pointSize, this._size);
+    gl.uniform4f(pointColor, ...this._color, 1.0);
 
     gl.drawArrays(gl.POINTS, 0, 1);
 
